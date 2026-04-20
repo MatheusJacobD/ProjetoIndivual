2e904900f03b7927767094c03501a7830589ca3d
MatheusJacobD <matheus.duarte@sptech.school>
2026-04-20 14:31:28 -0300

    feat: Site index base finalizado

--- tabuleiro.js
+++ tabuleiro.js
@@ -1,23 +1,28 @@
 const corVerde = "#779556";
 const corBege = "#EBECD0";
 
-function gerarTabuleiro(id, linhas, colunas) {
+function gerar(id, linhas, colunas) {
     let tabuleiro = document.getElementById(id);
-
     if (!tabuleiro) {
         return;
     }
+    let texto = tabuleiro.querySelector("span");
 
-    tabuleiro.innerHTML = "";
+    let grid = document.createElement("div");
+    grid.className = "gridTabuleiro";
+
+    grid.style.position = "absolute";
+    grid.style.inset = "0";
+    grid.style.display = "grid";
+    grid.style.gridTemplateColumns = `repeat(${colunas}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${linhas}, 1fr)`;
+    grid.style.zIndex = "1";
 
     for (let linha = 0; linha < linhas; linha++) {
-
         for (let coluna = 0; coluna < colunas; coluna++) {
-
             let casa = document.createElement("div");
-
-            // cor
             let cor;
+
             if ((linha + coluna) % 2 === 0) {
                 cor = corBege;
                 casa.className = "casaClara";
@@ -26,23 +31,27 @@ function gerarTabuleiro(id, linhas, colunas) {
                 casa.className = "casaEscura";
             }
 
-            // aumenta
-            let r = parseInt(cor[1] + cor[2], 16);
-            let g = parseInt(cor[3] + cor[4], 16);
-            let b = parseInt(cor[5] + cor[6], 16);
+            let r = parseInt(cor.slice(1, 3), 16);
+            let g = parseInt(cor.slice(3, 5), 16);
+            let b = parseInt(cor.slice(5, 7), 16);
 
-            // dim
             let opacidade = 1 - (coluna * 0.05);
-            if (opacidade < 0.15) {
-                opacidade = 0.15;
-            }
+            if (opacidade < 0.15) opacidade = 0.15;
 
-            casa.style.backgroundColor =
-                "rgba(" + r + ", " + g + ", " + b + ", " + opacidade + ")";
+            casa.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${opacidade})`;
 
-            tabuleiro.appendChild(casa);
+            grid.appendChild(casa);
         }
+    }
+
+    tabuleiro.innerHTML = "";
+    tabuleiro.appendChild(grid);
+
+    if (texto) {
+        texto.classList.add("textoTabuleiroFinal");
+        tabuleiro.appendChild(texto);
     }
 }
 
-gerarTabuleiro("tabuleiroHero", 3, 18);
+gerar("tabuleiroHero", 3, 18);
+gerar("tabuleiroFinal", 8, 18);
