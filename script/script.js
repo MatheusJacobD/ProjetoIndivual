a71c7d67e10284160324148e63bebb68fd7d47b3
MatheusJacobD <matheus.duarte@sptech.school>
2026-04-25 13:46:05 -0300

    feat: Validação implementada

---
+++ script/script.js
@@ -0,0 +1,48 @@
+function validar() {
+    let email = ipt_email.value;
+    let senha = ipt_senha.value;
+    let confirmar = ipt_confirmar.value;
+    let nome = ipt_nome.value;
+
+    let finalCorreto = "@sptech.school";
+    let valido = true;
+    let senhaValida = true;
+    let nomeValido = true;
+
+    if (email.length <= finalCorreto.length) {
+        valido = false;
+    } else {
+        for (let i = 0; i < finalCorreto.length; i++) {
+            let emailCount = email.length - finalCorreto.length + i;
+
+            if (email[emailCount] != finalCorreto[i]) {
+                valido = false;
+            }
+        }
+    }
+    
+    if (nome.length < 5) {
+        nomeValido = false;
+        erro_nome.innerHTML = "* nome deve ter mais que 5 caracteres";
+        erro_nome.style.color = "red";
+    }
+
+    if (senha.length < 6) {
+        erro_senha.innerHTML = "* senha deve ter mais que 6 caracteres";
+        erro_senha.style.color = "red";
+        senhaValida = false;
+    }
+
+    if (senha != confirmar) {
+        erro_senhaC.innerHTML = "* senhas não coincidem";
+        erro_senhaC.style.color = "red";
+        senhaValida = false;
+    }
+
+    if (valido) {
+        erro_email.innerHTML = "";
+    } else {
+        erro_email.innerHTML = "* email inválido";
+        erro_email.style.color = "red";
+    }
+}
