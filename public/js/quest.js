8f63290880af48874a23358512ec797e02582379
MatheusJacobD <matheus.duarte@sptech.school>
2026-04-30 13:51:22 -0300

    feat: Inicio da logica do questionario, base do questionario finalizado e imagens adicionadas

---
+++ public/js/quest.js
@@ -0,0 +1,33 @@
+gms = {
+    'Mikhail Tal' : 0,
+    'Garry Kasparov' : 0,
+    'Magnus Carlsen': 0,
+    'Anatoly Karpov' : 0,
+    'Bobby Fischer' : 0,
+    'Vladimir Kramnik' : 0,
+    'Hikaru Nakamura' : 0,
+    'Judit Polgar' : 0,
+    'Viswanathan Anand' : 0,
+    'Fabiano Caruana' : 0,
+    'Ding Liren' : 0,
+    'Alireza Firouzja' : 0,
+    'Wesley So' : 0,
+    'Tigran Petrosian' : 0,
+    'Hou Yifan' : 0
+}
+
+estilo = {
+    'agressivo' : 0,
+    'fechado': 0,
+    'estrategico' : 0
+}
+
+perguntas = {
+    'teste1' : ['resp1', 'resp2', 'resp3', 'resp4'],
+    'teste2' : ['resp1', 'resp2', 'resp3', 'resp4'],
+    'teste3' : ['resp1', 'resp2', 'resp3', 'resp4']
+}
+
+console.log(perguntas.teste1[1]);
+
+
